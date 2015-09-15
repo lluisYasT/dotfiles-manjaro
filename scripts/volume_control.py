cbfcabbddf97af9e60714a1193f39e7a7b219fac
Lluis <lluisyast@gmail.com>
2015-09-15 20:06:34 +0200

    Now it uses pulseaudio-ctl

--- scripts/volume_control.py
+++ scripts/volume_control.py
@@ -4,22 +4,20 @@ import sys
 
 import executor
 
-def get_active_sink():
-  return executor.run('pacmd list-sinks | grep "* index" | awk \'{print $3}\'')[0]
 
 def get_volume():
-  return executor.run('amixer -D pulse get Master | grep -o "\[.*%\]" | grep -o "[0-9]*" | head -n1')[0]
+  return executor.run('pulseaudio-ctl current | grep -o "[0-9]*" | head -n1')[0]
 
 def set_volume(percentage):
-  executor.run('pactl set-sink-volume ' + get_active_sink() + ' ' + str(percentage) + '%')
+  executor.run('pulseaudio-ctl set ' + str(percentage))
   emit_signal()
 
 def toggle_volume():
-  executor.run('amixer -D pulse set Master Playback Switch toggle')
+  executor.run('amixer set Master Playback Switch toggle')
   emit_signal()
 
 def is_muted():
-  return not executor.run('amixer -D pulse get Master | grep -o "\[on\]" | head -n1')[0]
+  return not executor.run('amixer get Master | grep -o "\[on\]" | head -n1')[0]
 
 def write(message):
   sys.stdout.write(message)
@@ -29,8 +27,8 @@ def trim_to_range(volume):
   volume = int(volume)
   if volume < 0:
     volume = 0
-  elif volume > 200:
-    volume = 200
+  elif volume > 95:
+    volume = 100
   return volume
 
 def status():
@@ -48,11 +46,11 @@ if __name__ == '__main__':
   if command == 'set':
     set_volume(trim_to_range(sys.argv[2]))
   elif command == 'up':
-    new_volume = trim_to_range(int(get_volume()) + int(sys.argv[2]))
-    set_volume(new_volume)
+      executor.run('pulseaudio-ctl up')
+      emit_signal()
   elif command == 'down':
-    new_volume = trim_to_range(int(get_volume()) - int(sys.argv[2]))
-    set_volume(new_volume)
+      executor.run('pulseaudio-ctl down')
+      emit_signal()
   elif command == 'toggle':
     toggle_volume()
   elif command == 'read':
